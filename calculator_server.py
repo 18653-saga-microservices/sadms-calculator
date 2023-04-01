a6a0a8a2b728ddaa0a2968ad0c27ed310f483f0c
Christian Eilers <christian@eilers.cl>
2023-03-31 17:03:14 -0700

    finished registry handshake

--- calculator_server.py
+++ calculator_server.py
@@ -2,6 +2,7 @@ from concurrent import futures
 import grpc
 import calculator_pb2
 import calculator_pb2_grpc
+import registry_pb2_grpc, registry_pb2
 
 
 class Calculator(calculator_pb2_grpc.CalculatorServicer):
@@ -29,9 +30,18 @@ class Calculator(calculator_pb2_grpc.CalculatorServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     calculator_pb2_grpc.add_CalculatorServicer_to_server(Calculator(), server)
+    register_microservice()
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
+
+def register_microservice():
+    with grpc.insecure_channel('localhost:50123') as channel:
+        stub = registry_pb2_grpc.RegistryServiceStub(channel)
+        register_request = registry_pb2.RegistryRequest(serviceName="calculator", serviceAddress="127.0.0.1", servicePort=50051)
+        response = stub.RegisterService(register_request)
+        print("registered microservice", response)
 
 
 if __name__ == '__main__':

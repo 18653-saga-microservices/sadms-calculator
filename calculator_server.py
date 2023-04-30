db04e4992264c20027221ccffd4e7d8311e41e08
Yusheng Hu <43050926+vcaptainv@users.noreply.github.com>
2023-04-29 22:21:14 -0700

    add error handling

--- calculator_server.py
+++ calculator_server.py
@@ -9,43 +9,36 @@ import registry_pb2
 class Calculator(calculator_pb2_grpc.CalculatorServicer):
     def Add(self, request, context):
         result = request.first_number + request.second_number
-        response = calculator_pb2.Response(result=result)
+        response = calculator_pb2.Response(
+            result=result, status=calculator_pb2.Status(code=0, message="Success"))
         return response
 
     def Multiply(self, request, context):
         result = request.first_number * request.second_number
-        response = calculator_pb2.Response(result=result)
+        response = calculator_pb2.Response(
+            result=result, status=calculator_pb2.Status(code=0, message="Success"))
         return response
 
     def Subtract(self, request, context):
         result = request.first_number - request.second_number
-        response = calculator_pb2.Response(result=result)
+        response = calculator_pb2.Response(
+            result=result, status=calculator_pb2.Status(code=0, message="Success"))
         return response
 
     def Divide(self, request, context):
+        if request.second_number == 0:
+            return calculator_pb2.Response(status=calculator_pb2.Status(code=1, message="Division by zero error"))
         result = request.first_number / request.second_number
-        response = calculator_pb2.Response(result=result)
+        response = calculator_pb2.Response(
+            result=result, status=calculator_pb2.Status(code=0, message="Success"))
         return response
 
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     calculator_pb2_grpc.add_CalculatorServicer_to_server(Calculator(), server)
-    register_microservice() # TODO get response and error handle.
-    # if error:
-    #    print(error)
-    #    exit(0)
+    register_microservice()
     server.add_insecure_port('[::]:50051')
-    with grpc.insecure_channel('localhost:40001') as channel:
-        stub = registry_pb2_grpc.RegistryStub(channel)
-        serviceAddress = 'localhost'
-        servicePort = 50051
-        serviceName = 'Calculator'
-        registryRequest = registry_pb2.RegistryRequest(
-            serviceName=serviceName, serviceAddress=serviceAddress,
-            servicePort=servicePort)
-        response = stub.Register(registryRequest)
-        print(response.serviceID)
     server.start()
     server.wait_for_termination()
 
@@ -56,7 +49,13 @@ def register_microservice():
         register_request = registry_pb2.RegistryRequest(
             serviceName="calculator", serviceAddress="127.0.0.1", servicePort=50051)
         response = stub.RegisterService(register_request)
-        print("registered microservice", response)
+
+        if response.status.code == 0:
+            print(
+                f"Registered microservice {response.serviceName} successfully")
+        else:
+            print(
+                f"Failed to register microservice {response.serviceName}: {response.status.message}")
 
 
 if __name__ == '__main__':
